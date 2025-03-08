20a806b438b67dcfc645386f4a722fcad906cf26
artyommukhin <artyom.mukhin@gmail.com>
2025-03-08 09:55:04 +0000

    Update gh-pages to output generated at 17d965f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "version.json": "ad680a829830c20a0e407d568fedae8e",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"main.dart.js": "295ef7462fa7bd99be9a0a8eac275035",
+"main.dart.js": "844ebb94f6db91ef21c7bd400fb641be",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "210fce35ccb135c63f17233f3d6748e2",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "808b9bb14f7bb90261c496557618ae22"};
+"flutter_bootstrap.js": "3fc2b1ac33c0586df1f15f3ba77dd016"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
